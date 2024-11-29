338638ac2974c6e6aa4296bc5b197743675b0ec4
Lê Yên <140315513+leyen1403@users.noreply.github.com>
2024-11-29 15:24:42 +0700

    Update frm_dangNhap.Designer.cs

--- APP/GUI/frm_dangNhap.Designer.cs
+++ APP/GUI/frm_dangNhap.Designer.cs
@@ -127,6 +127,7 @@
             // 
             // frm_dangNhap
             // 
+            this.AcceptButton = this.btn_dangnhap;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
